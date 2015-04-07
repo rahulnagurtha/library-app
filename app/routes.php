21cc1dcb983e02f4ba7cf9a4bb2f8655ab2b9fc5
Aneesh Dash <aneeshdash@gmail.com>
2015-04-07 17:19:26 +0530

    Revert "added admin lost_book and updates"

--- app/routes.php
+++ app/routes.php
@@ -10,10 +10,25 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+/*****************************************     UserController     *****************************************************/
+Route::get('home', ['as' => 'home', 'uses' => 'UserController@home']);
+Route::get('accounts', ['as' => 'accounts', 'uses' => 'UserController@accounts']);
+Route::get('wish_list', ['as' => 'wish_list', 'uses' => 'UserController@wish_list']);
+Route::get('queued_books', ['as' => 'queued_books', 'uses' => 'UserController@queued_books']);
+Route::get('contacts', ['as' => 'contacts', 'uses' => 'UserController@contacts']);
+Route::get('lost_book', ['as' => 'lost_book', 'uses' => 'UserController@lost_book']);
+Route::get('donate_book', ['as' => 'donate_book', 'uses' => 'UserController@donate_book']);
+Route::get('login', ['as' => 'login', 'uses' => 'UserController@login']);
+/**********************************************************************************************************************/
 
-Route::get('/', ['as' => 'home', 'uses' => 'HomeController@showWelcome']);
+/*AdminController*/
+Route::get('admin', ['as' => 'admin', 'uses' => 'AdminController@hello']);
 Route::get('user', ['as' => 'user', 'uses' => 'UserController@hello']);
-Route::get('admin', ['as' => 'admin', 'uses' => 'AdminController@master']);
-Route::get('test', ['as' => 'home', 'uses' => 'AdminController@abcd']);
-Route::get('admin/update', ['as' => 'adminupdate', 'uses' => 'AdminController@update']);
-Route::get('admin/lostbook', ['as' => 'adminlostbook', 'uses' => 'AdminController@lost']);
+Route::get('admin/login', ['as' => 'adminlogin', 'uses' => 'AdminController@login']);
+Route::post('admin/login', ['as' => 'adminlogin', 'uses' => 'AdminController@postlogin']);
+Route::get('admin/logout', ['as' => 'adminlogout', 'uses' => 'AdminController@logout']);
+
+Route::group(array('before'=>'auth.admin'),function()
+{
+    Route::get('test', ['as' => 'home', 'uses' => 'AdminController@abcd']);
+});
